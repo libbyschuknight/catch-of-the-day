278425f60b9ad2ee4a08e7a66bcf190b94af2013
Libby Schumacher-Knight <libby@fluxfederation.com>
2019-05-03 13:32:39 +1200

    Begin to add state (vid 13)

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. take a copy of existing state (state should be immutable)
+    const fishes = { ...this.state.fishes };
+    // 2. add our new fish to fishes var
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object to state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
