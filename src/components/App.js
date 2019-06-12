91ad566aa545c3f346a08e18c61380981e41a6f6
Libby Schumacher-Knight <libby@fluxfederation.com>
2019-06-12 18:49:54 +1200

    Updating our Order State (vid 16), some of vid 17

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,18 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+  // state at the top
   state = {
     fishes: {},
     order: {}
   };
 
+  // lifecycle events
+
+  // custom stuff
   addFish = fish => {
     // 1. take a copy of existing state (state should be immutable)
     const fishes = { ...this.state.fishes };
@@ -23,13 +28,35 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = key => {
+    // take copy of state
+    const order = { ...this.state.order };
+
+    // either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+
+    // call setState to update our state object
+    this.setState({ order });
+  };
+
+  // finally render method
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- src/components/Order.js
+++ src/components/Order.js
@@ -3,10 +3,10 @@ import React from 'react';
 class Order extends React.Component {
   render() {
     return (
-      <div className="order">
-        Order
+      <div className="order-wrap">
+        <h2>Order</h2>
       </div>
-    )
+    );
   }
 }
 
