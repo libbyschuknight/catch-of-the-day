06e8f1e0caef585cb0d5d9e25a4e88840a9aa9b4
Libby Schumacher-Knight <libby@fluxfederation.com>
2019-06-14 16:23:06 +1200

    Bi-directional Data Flow and Live State Editing (vid 20)

--- src/components/App.js
+++ src/components/App.js
@@ -48,6 +48,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  updateFish = (key, updatedFish) => {
+    // 1 take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2 update that state
+    fishes[key] = updatedFish;
+    // 3 set that to state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -83,7 +92,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
