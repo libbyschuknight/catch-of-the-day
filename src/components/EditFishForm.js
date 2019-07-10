32fc9f5508f6cb7bbe8d772b44d70f70d9176019
Libby Schumacher-Knight <libby@fluxfederation.com>
2019-07-10 17:54:03 +1200

    Component Validation with PropTypes (vid 23)

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class EditFishForm extends React.Component {
+  static propTypes = {
+    // fish: PropTypes.instanceOf(Fish)
+    fish: PropTypes.shape({
+      name: PropTypes.string,
+      price: PropTypes.number,
+      status: PropTypes.string,
+      desc: PropTypes.string,
+      image: PropTypes.string
+    }),
+    index: PropTypes.string,
+    updateFish: PropTypes.func,
+    deleteFish: PropTypes.func
+  };
   handleChange = event => {
     // update the fish
     const updatedFish = {
