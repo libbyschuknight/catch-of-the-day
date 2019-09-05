4c9a446996bc01aadbf6f487e9e68232f1e6bdaf
Libby Schumacher-Knight <libby@fluxfederation.com>
2019-09-06 09:10:05 +1200

    Start adding authentication using firebase (vid 24)

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -12,7 +13,12 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   };
 
+  authenticate = () => {
+    alert('yay');
+  };
+
   render() {
+    return <Login authenticate={this.authenticate} />;
     return (
       <div className="inventory">
         <h2>Inventory</h2>
